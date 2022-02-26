21da27eee284404f6797d8b1c2868225d152ef21
Bounar64 <rd.bounar@gmail.com>
2022-02-26 21:10:16 +0100

    ajout info captcha au click

--- assets/app.js
+++ assets/app.js
@@ -17,58 +17,73 @@ const buttons = document.querySelectorAll('.button');
 const button_card = document.querySelectorAll('.button-card');
 const plus = document.querySelectorAll('.bi-plus-lg');
 
-buttons[0].onclick = function() {
-    button_card[0].classList.toggle('active');
-    plus[0].classList.toggle('cross');
-    button_card[1].classList.remove('active');  
-    button_card[2].classList.remove('active');  
-    button_card[3].classList.remove('active');  
-    button_card[4].classList.remove('active');  
-    button_card[5].classList.remove('active');  
-}
-buttons[1].onclick = function() {
-    button_card[1].classList.toggle('active');
-    plus[1].classList.toggle('cross');
-    button_card[0].classList.remove('active');  
-    button_card[2].classList.remove('active');  
-    button_card[3].classList.remove('active');  
-    button_card[4].classList.remove('active');  
-    button_card[5].classList.remove('active');  
-}
-buttons[2].onclick = function() {
-    button_card[2].classList.toggle('active');
-    plus[2].classList.toggle('cross');
-    button_card[0].classList.remove('active');  
-    button_card[1].classList.remove('active');  
-    button_card[3].classList.remove('active');  
-    button_card[4].classList.remove('active');  
-    button_card[5].classList.remove('active');  
-}
-buttons[3].onclick = function() {
-    button_card[3].classList.toggle('active');
-    plus[3].classList.toggle('cross');
-    button_card[0].classList.remove('active');  
-    button_card[1].classList.remove('active');  
-    button_card[2].classList.remove('active');  
-    button_card[4].classList.remove('active');  
-    button_card[5].classList.remove('active');  
-}
-buttons[4].onclick = function() {
-    button_card[4].classList.toggle('active');
-    plus[4].classList.toggle('cross');
-    button_card[0].classList.remove('active');  
-    button_card[1].classList.remove('active');  
-    button_card[2].classList.remove('active');  
-    button_card[3].classList.remove('active');  
-    button_card[5].classList.remove('active');  
-}
-buttons[5].onclick = function() {
-    button_card[5].classList.toggle('active');
-    plus[5].classList.toggle('cross');
-    button_card[0].classList.remove('active');  
-    button_card[1].classList.remove('active');  
-    button_card[2].classList.remove('active');  
-    button_card[3].classList.remove('active');  
-    button_card[4].classList.remove('active'); 
+const pathname = window.location.pathname; // pourquoi la compilation ne reconnaît pas toute les pages.. 
+
+if(window.location.pathname == "/") {
+
+    buttons[0].onclick = function() {
+        button_card[0].classList.toggle('active');
+        plus[0].classList.toggle('cross');
+        button_card[1].classList.remove('active');  
+        button_card[2].classList.remove('active');  
+        button_card[3].classList.remove('active');  
+        button_card[4].classList.remove('active');  
+        button_card[5].classList.remove('active');  
+    }
+
+    buttons[1].onclick = function() {
+        button_card[1].classList.toggle('active');
+        plus[1].classList.toggle('cross');
+        button_card[0].classList.remove('active');  
+        button_card[2].classList.remove('active');  
+        button_card[3].classList.remove('active');  
+        button_card[4].classList.remove('active');  
+        button_card[5].classList.remove('active');  
+    }
+    buttons[2].onclick = function() {
+        button_card[2].classList.toggle('active');
+        plus[2].classList.toggle('cross');
+        button_card[0].classList.remove('active');  
+        button_card[1].classList.remove('active');  
+        button_card[3].classList.remove('active');  
+        button_card[4].classList.remove('active');  
+        button_card[5].classList.remove('active');  
+    }
+    buttons[3].onclick = function() {
+        button_card[3].classList.toggle('active');
+        plus[3].classList.toggle('cross');
+        button_card[0].classList.remove('active');  
+        button_card[1].classList.remove('active');  
+        button_card[2].classList.remove('active');  
+        button_card[4].classList.remove('active');  
+        button_card[5].classList.remove('active');  
+    }
+    buttons[4].onclick = function() {
+        button_card[4].classList.toggle('active');
+        plus[4].classList.toggle('cross');
+        button_card[0].classList.remove('active');  
+        button_card[1].classList.remove('active');  
+        button_card[2].classList.remove('active');  
+        button_card[3].classList.remove('active');  
+        button_card[5].classList.remove('active');  
+    }
+    buttons[5].onclick = function() {
+        button_card[5].classList.toggle('active');
+        plus[5].classList.toggle('cross');
+        button_card[0].classList.remove('active');  
+        button_card[1].classList.remove('active');  
+        button_card[2].classList.remove('active');  
+        button_card[3].classList.remove('active');  
+        button_card[4].classList.remove('active'); 
+    }
 }
 /************* Création de l'accordéon du FAQ *************/
+
+/************* Affichage des infos captcha au click du lien *************/
+if(window.location.pathname == "/login") {
+    document.querySelector('.link-captcha-info').onclick = function() {
+        document.querySelector('.link-captcha-info').classList.add('active');
+        document.querySelector('.captcha-info').classList.add('active');
+    }
+}
+/************* Affichage des infos captcha au click du lien *************/
